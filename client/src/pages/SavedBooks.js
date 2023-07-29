e15f17eaed324ab2c8f9a0945b711a1b9b8e066a
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-07-29 07:02:31 -0600

    "add punctuation"

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import React from "react";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
   const [userData, setUserData] = useQuery(GET_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-  
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -29,7 +22,7 @@ const SavedBooks = () => {
       const response = await removeBook(bookId, token);
 
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error("something went wrong!");
       }
 
       const updatedUser = await response.json();
@@ -40,41 +33,51 @@ const SavedBooks = () => {
       console.error(err);
     }
   };
+};
 
-
-    <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </div>
-      <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
-
+<>
+  <div fluid className="text-light bg-dark p-5">
+    <Container>
+      <h1>Viewing saved books!</h1>
+    </Container>
+  </div>
+  <Container>
+    <h2 className="pt-5">
+      {userData.savedBooks.length
+        ? `Viewing ${userData.savedBooks.length} saved ${
+            userData.savedBooks.length === 1 ? "book" : "books"
+          }:`
+        : "You have no saved books!"}
+    </h2>
+    <Row>
+      {userData.savedBooks.map((book) => {
+        return (
+          <Col md="4">
+            <Card key={book.bookId} border="dark">
+              {book.image ? (
+                <Card.Img
+                  src={book.image}
+                  alt={`The cover for ${book.title}`}
+                  variant="top"
+                />
+              ) : null}
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <p className="small">Authors: {book.authors}</p>
+                <Card.Text>{book.description}</Card.Text>
+                <Button
+                  className="btn-block btn-danger"
+                  onClick={() => handleDeleteBook(book.bookId)}
+                >
+                  Delete this Book!
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  </Container>
+</>;
 
 export default SavedBooks;
